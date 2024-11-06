1e28deceb04fde42ff65259554520f6a949e611a
David Drake <david@randomdrake.com>
2024-11-05 16:26:07 -0800

    Removing unused var

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   EnvelopeIcon,
@@ -11,7 +10,7 @@ import { Button } from '@/app/ui/button';
 import { createCustomer, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default function Form() {
   const initialState: State = { message: "", errors: {} };
   const [state, formAction] = useActionState(createCustomer, initialState);
 
