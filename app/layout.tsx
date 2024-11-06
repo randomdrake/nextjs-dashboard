4c8a352cfc6059a2901ac01759cae2468691fae9
David Drake <david@randomdrake.com>
2024-11-05 17:21:57 -0800

    Adding analytics 📊

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
