17e6f6d904780250890deaf91116fe5d83af41cd
David Drake <david@randomdrake.com>
2024-11-06 09:39:09 -0800

    Adding required fields

--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -30,6 +30,7 @@ export default function Form() {
                 placeholder="Jane Doe"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="name-error"
+                required
               />
               <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -57,6 +58,7 @@ export default function Form() {
                 placeholder="email@example.com"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 aria-describedby="email-error"
+                required
               />
               <EnvelopeIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
