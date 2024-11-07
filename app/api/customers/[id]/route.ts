7cf30bc47560f751b1d26772824b3932051baaec
David Drake <david@randomdrake.com>
2024-11-07 15:23:21 -0800

    Use Promise where necessary

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -3,7 +3,7 @@ import { fetchCustomerById } from 'app/lib/data';
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
 
