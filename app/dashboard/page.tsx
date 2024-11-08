a7f4255e0ce1ec6c0a263fdc525d30aa96e290b3
David Drake <david@randomdrake.com>
2024-11-07 20:30:28 -0800

    Getting rid of (overview)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import RevenueChart from 'app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
